267921f3fb23b1a7cb62cd3def272f1b49b90856
cate97819 <kate.pfo@gmail.com>
2025-06-26 16:41:47 +0900

    ロゴ差替え

--- app/components/section/Footer.tsx
+++ app/components/section/Footer.tsx
@@ -4,7 +4,9 @@ const Footer = () => {
   return (
     <footer className='w-full bg-slate-900 py-10 px-4'>
       <div className='max-w-[1200px] w-full mx-auto'>
-        <h1 className='text-white text-2xl py-6'>LOGO</h1>
+        <h1 className='text-white text-2xl py-6'>
+          <img src="/images/evelogo_text001.png" alt="さがそうイベントの場 EVENT NOVA" className='max-w-[180px] bg-white p-4' />
+        </h1>
         <ul className='text-white text-sm underline flex flex-col gap-4 text-sm'>
           <li>
             <a href="https://www.tohgashi.co.jp/pdf/kojinjouhou.pdf" target='_blank'>
