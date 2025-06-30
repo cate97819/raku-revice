83755a22b44e857f8bfc8e0c7a46b571adc9de5d
cate <kate.pfo@gmail.com>
2025-06-30 17:47:18 +0900

    タイトル、ディスクリプション修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import Header from "./components/section/Header";
 const notoSansJP = Noto_Sans_JP({subsets: ["latin"], weight: ["100", "300", "500", "700", "900"]});
 
 export const metadata: Metadata = {
-  title: "EVENT NOVA｜さがそう、イベントの場。",
-  description: "",
+  title: "ビジネスイベントの担当者向けに最適な会場を簡単に検索。｜イベントの場を探すなら「イベントノバ EVENT NOVA」",
+  description: "イベントの場を探すなら「イベントノバ EVENT NOVA」！ビジネスイベントの担当者向けに最適な会場を簡単に検索。役立つ資料やホワイトペーパーもダウンロード可能。プロのアドバイスも受けられ、イベント成功への第一歩をサポートします。",
   icons: [{ rel: 'icon', url: "/nova_favicon.ico" }],
 };
 
